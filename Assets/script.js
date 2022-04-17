f4badfbfbf062abc68237071428a10c043bbc639
JasonKim0122 <JasonKim0122@gmail.com>
2022-04-16 23:23:12 -0500

    added modals for tickets and donate tab

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,67 @@
 //Modal
 var aboutUsModal = document.getElementById("aboutUs");
-var modalBg = document.querySelector(".modal-background");
-var modal = document.querySelector(".modal");
+var ticketsModal = document.getElementById("ticketsModalId");
+var donateModal = document.getElementById("donateModalId");
+var modalBg = document.querySelector(".aboutUsBg");
+var modalBg1 = document.querySelector(".ticketsBg");
+var modalBg2 = document.querySelector(".donateBg");
+var modalAbout = document.querySelector(".aboutUsModal");
+var modalTicket = document.querySelector(".ticketsModal");
+var modalDonate = document.querySelector(".donateModal");
 var exitBtn = document.getElementById("exitBtn");
 var exitBtn2 = document.getElementById("deleteBtn");
+var exitBtn3 =document.getElementById("exitBtnTickets"); 
+var exitBtn4= document.getElementById("deleteBtnTickets");
+var exitBtn5 = document.getElementById("exitBtnDonate");
+var exitBtn6 = document.getElementById("deleteBtnDonate");
 
+//Eventlistener for aboutUs tab
 aboutUsModal.addEventListener("click", () => {
-    modal.classList.add("is-active");
+    modalAbout.classList.add("is-active");
 });
 
 modalBg.addEventListener("click", () => {
-    modal.classList.remove("is-active");
-})
+    modalAbout.classList.remove("is-active");
+});
 
 exitBtn.addEventListener("click", () => {
-    modal.classList.remove("is-active");
-})
+    modalAbout.classList.remove("is-active");
+});
 
 exitBtn2.addEventListener("click", () => {
-    modal.classList.remove("is-active");
+    modalAbout.classList.remove("is-active");
+});
+
+//Event listener for tickets tab
+ticketsModal.addEventListener("click", () => {
+    modalTicket.classList.add("is-active");
+});
+
+modalBg1.addEventListener("click", () => {
+    modalTicket.classList.remove("is-active");
 })
+
+exitBtn3.addEventListener("click", () => {
+    modalTicket.classList.remove("is-active");
+});
+
+exitBtn4.addEventListener("click", () => {
+    modalTicket.classList.remove("is-active");
+});
+
+//Event Listener for Donation tab
+donateModal.addEventListener("click", () => {
+    modalDonate.classList.add("is-active");
+});
+
+modalBg2.addEventListener("click", () => {
+    modalDonate.classList.remove("is-active");
+})
+
+exitBtn5.addEventListener("click", () => {
+    modalDonate.classList.remove("is-active");
+});
+
+exitBtn6.addEventListener("click", () => {
+    modalDonate.classList.remove("is-active");
+});
